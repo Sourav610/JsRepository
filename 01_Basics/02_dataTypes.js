73d70bb06e23a616d4910fe0b5015570b466d773
Sourav kumar <80942384+Sourav610@users.noreply.github.com>
2024-09-11 20:17:19 +0000

    dataypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+//datatypse
+//number => 2 to power 53
+//bigint
+//string => "" or ''
+//boolean => true/false
+//null => standalone value
+//undefine => when value is not define
+//symbol => unique
+
+//object 
+
+console.log(typeof "sourav") // typeof will tell what type the variable is
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+ 
