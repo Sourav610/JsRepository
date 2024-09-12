d0a937ad48fa4ec5a86cb446166a3aa2081f33f2
Sourav kumar <80942384+Sourav610@users.noreply.github.com>
2024-09-12 19:31:58 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33
+let check = "434af"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+let valueInCheck = Number(check)
+console.log(valueInNumber)
+console.log(valueInCheck)
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//"" => false
+//"sdf" => true
