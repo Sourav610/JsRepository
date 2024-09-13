69aedc43737beab68f9d9bda51fe04236ce1b108
Sourav kumar <80942384+Sourav610@users.noreply.github.com>
2024-09-13 20:12:02 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -16,3 +16,42 @@ console.log(booleanIsLoggedIn)
 
 //"" => false
 //"sdf" => true
+
+//******************* Operations ***************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str = "hello"
+let str2 = " sourav"
+let str3 = str + str2
+console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") // 12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2")// but it will give 32 as string is last so it add first then add the string
+
+
+// console.log(true)
+// console.log(+true) // it conver to value but don't use it
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 1
+++gameCounter // prefix operator
+gameCounter++ // postfix operator
+
+
+
+
+
