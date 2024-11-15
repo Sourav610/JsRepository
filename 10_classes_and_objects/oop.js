3f57e910ab53b1822263352dfbead3fc76c2bcdc
Sourav kumar <80942384+Sourav610@users.noreply.github.com>
2024-11-15 18:09:29 +0000

    new keywords

--- 10_classes_and_objects/oop.js
+++ 10_classes_and_objects/oop.js
@@ -12,3 +12,19 @@ const user = {
 console.log(user.username);
 // console.log(user.getUserDetails());
 console.log(this)
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    function getUserDetails(){
+        console.log("Gerring user details")
+    }
+}
+
+//new key word used to create new instance using constructor function.
+const userOne = new User("hitesh",12, true)
+const userTwo = new User("sourav",22,false);
+console.log(userOne)
+console.log(userTwo)
